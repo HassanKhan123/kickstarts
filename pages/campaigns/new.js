b2787937b12951f781bc4e9755430bc99e9e039f
HassanKhan123 <hk2602809@gmail.com>
2021-05-18 23:08:43 +0500

    form submit handler added

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -2,13 +2,24 @@ import React, { useState } from 'react';
 import { Form, Button, Input } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 const CampaignNew = () => {
   const [minimumContribution, setMinimumContribution] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods.createCampaign(minimumContribution).send({
+      from: accounts[0],
+    });
+  };
   return (
     <Layout>
       <h3>Create a Campaign</h3>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input
