53f39af53895f78b0c7c167d67939eb9eaf6df25
HassanKhan123 <hk2602809@gmail.com>
2021-05-18 23:40:47 +0500

    form validation added

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,14 +2,19 @@ import Web3 from 'web3';
 
 let web3;
 
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  web3 = new Web3(window.web3.currentProvider);
-} else {
-  const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/2107de90a19f4dd69c0eef59805a707e'
-  );
+const ethEnabled = async () => {
+  if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    // await window.ethereum.sendAsync('eth_requestAccounts');
+    web3 = new Web3(window.ethereum);
+  } else {
+    const provider = new Web3.providers.HttpProvider(
+      'https://rinkeby.infura.io/v3/2107de90a19f4dd69c0eef59805a707e'
+    );
 
-  web3 = new Web3(provider);
-}
+    web3 = new Web3(provider);
+  }
+};
+
+ethEnabled();
 
 export default web3;
--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
@@ -7,14 +7,21 @@ import web3 from '../../ethereum/web3';
 
 const CampaignNew = () => {
   const [minimumContribution, setMinimumContribution] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods.createCampaign(minimumContribution).send({
-      from: accounts[0],
-    });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      await factory.methods.createCampaign(minimumContribution).send({
+        from: accounts[0],
+      });
+    } catch (error) {
+      setErrorMessage(error.message);
+      console.log(error.message);
+    }
   };
   return (
     <Layout>
@@ -29,7 +36,12 @@ const CampaignNew = () => {
             onChange={(e) => setMinimumContribution(e.target.value)}
           />
         </Form.Field>
-
+        <Message
+          error
+          header='Oops!'
+          content={errorMessage}
+          visible={!!errorMessage}
+        />
         <Button primary>Create!</Button>
       </Form>
     </Layout>
