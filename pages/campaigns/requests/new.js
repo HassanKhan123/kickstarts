3f172956a9bde52bc3a3a2c76569c0dd95b06a0f
HassanKhan123 <hk2602809@gmail.com>
2021-05-19 23:21:02 +0500

    request form created

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button, Message } from 'semantic-ui-react';
 
-const RequestNew = () => {
+import web3 from '../../../ethereum/web3';
+import Campaign from '../../../ethereum/campaign';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
+
+const RequestNew = (props) => {
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [recipient, setRecipient] = useState('');
+
   return (
-    <div>
-      <h1>New Request</h1>
-    </div>
+    <Layout>
+      <h3>Create a Request</h3>
+      <Form>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+        </Form.Field>
+
+        <Button primary>Create!</Button>
+      </Form>
+    </Layout>
   );
+};
+
+RequestNew.getInitialProps = async (props) => {
+  const { address } = props.query;
+  return { address };
 };
 
 export default RequestNew;
