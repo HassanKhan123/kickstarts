767af5064432cfcce52ba630da4af784aa60b2cb
HassanKhan123 <hk2602809@gmail.com>
2021-05-20 07:50:49 +0500

    row status styling added

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -6,24 +6,51 @@ import Campaign from '../ethereum/campaign';
 import { Router } from '../routes';
 
 const RequestRow = (props) => {
-  const [loading, setLoading] = useState(false);
+  const [approveLoading, setApproveLoading] = useState(false);
+  const [finalizeLoading, setFinalizeLoading] = useState(false);
   const { Row, Cell } = Table;
   const { id, request, approversCount, address } = props;
 
-  const approveRequestHandler = async () => {
-    setLoading(true);
-    const accounts = await web3.eth.getAccounts();
-    const campaign = Campaign(address);
-    await campaign.methods.approveRequest(id).send({
-      from: accounts[0],
-    });
+  const readyToFinalize = request.approvalsCount > approversCount / 2;
 
-    setLoading(false);
+  const approveRequestHandler = async () => {
+    setApproveLoading(true);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const campaign = Campaign(address);
+      await campaign.methods.approveRequest(id).send({
+        from: accounts[0],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    setApproveLoading(false);
     Router.replaceRoute(`/campaigns/${address}/requests`);
   };
 
+  const finalizeRequestHandler = async () => {
+    setFinalizeLoading(true);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const campaign = Campaign(address);
+      await campaign.methods.finalizeRequest(id).send({
+        from: accounts[0],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    setFinalizeLoading(false);
+  };
+
   return (
-    <Row>
+    <Row
+      disabled={request.complete}
+      positive={readyToFinalize && !request.complete}
+    >
       <Cell>{id}</Cell>
       <Cell>{request.description}</Cell>
       <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
@@ -32,16 +59,29 @@ const RequestRow = (props) => {
         {request.approvalsCount} / {approversCount}
       </Cell>
       <Cell>
-        <Button
-          color='green'
-          basic
-          onClick={approveRequestHandler}
-          loading={loading}
-        >
-          Approve
-        </Button>
+        {request.complete ? null : (
+          <Button
+            color='green'
+            basic
+            onClick={approveRequestHandler}
+            loading={approveLoading}
+          >
+            Approve
+          </Button>
+        )}
       </Cell>
-      <Cell></Cell>
+      <Cell>
+        {request.complete ? null : (
+          <Button
+            color='teal'
+            basic
+            onClick={finalizeRequestHandler}
+            loading={finalizeLoading}
+          >
+            Finalize
+          </Button>
+        )}
+      </Cell>
     </Row>
   );
 };
